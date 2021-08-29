a563a1ccb9ceb1b251bd7e5544b2c5290e8d8868
SebastianPrivas <sebastian.privas@aon.at>
2021-08-29 11:33:20 +0200

    bug: fixed npm scope conflict

--- libs/ui/src/lib/ui/ui.module.ts
+++ libs/ui/src/lib/ui/ui.module.ts
@@ -3,37 +3,38 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import { NavComponent } from './components/nav/nav.component';
-import { MatListModule } from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import {
   _MatMenuDirectivesModule,
   MatMenuModule,
 } from '@angular/material/menu';
 import { FlexModule } from '@angular/flex-layout';
 import { MatRippleModule } from '@angular/material/core';
-import { BlogsComponent } from './components/blogs/blogs.component';
-import { BlogService } from './services/blog.service';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogService } from '../services/blog.service';
+import { MatListModule } from '@angular/material/list';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog/:id', component: BlogsComponent },
+];
 
 @NgModule({
   imports: [
     CommonModule,
     MatSidenavModule,
-    MatToolbarModule,
     MatCardModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatListModule,
     MatIconModule,
-    RouterModule,
+    MatToolbarModule,
     _MatMenuDirectivesModule,
     MatMenuModule,
     FlexModule,
     MatRippleModule,
+    RouterModule.forRoot(appRoutes),
+    MatListModule
   ],
   declarations: [NavComponent, HomeComponent, BlogsComponent],
   providers: [BlogService],
